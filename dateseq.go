0f7339c985d020c38f007f41944a63233d4d12fc
Peter Hickman <peterhickman386@googlemail.com>
2024-08-06 18:24:36 +0100

    reverse sequences and usage

--- dateseq.go
+++ dateseq.go
@@ -11,6 +11,29 @@ import (
 var starting_time time.Time
 var ending_time time.Time
 var days time.Duration
+var direction int
+
+func usage() {
+	fmt.Println("Like seq but for dates")
+	fmt.Println()
+	fmt.Println("dateseq 2024-01-01 2024-01-10")
+	fmt.Println("  will list all the dates from 2024-01-01 to 2024-01-10 inclusive")
+	fmt.Println()
+	fmt.Println("dateseq 2024-01-10 2024-01-01")
+	fmt.Println("  will list all the dates from 2024-01-01 to 2024-01-10 in the reverse order")
+	fmt.Println()
+	fmt.Println("dateseq 2024-01-01 7 2024-12-31")
+	fmt.Println("  will list every 7th date from 2024-01-01 up to 2024-12-31 but")
+	fmt.Println("  may not include the final date")
+	fmt.Println()
+	fmt.Println("dateseq 2024-12-31 7 2024-01-01")
+	fmt.Println("  will list every 7th date from 2024-12-31 down to 2024-01-01 but")
+	fmt.Println("  may not include the first date")
+	fmt.Println()
+	fmt.Println("Basically the step value is always a positive value")
+
+	os.Exit(1)
+}
 
 func init() {
 	flag.Parse()
@@ -26,14 +49,25 @@ func init() {
 	case 3:
 		// This is ok
 	default:
-		println("Wrong arguments")
-		os.Exit(1)
+		usage()
 	}
 
 	s, err := time.Parse(time.DateOnly, flag.Arg(start_index))
 	if err != nil {
 		fmt.Printf("Could not parse %s as a date: %s\n", flag.Arg(start_index), err)
 		os.Exit(2)
+	}
+
+	e, err := time.Parse(time.DateOnly, flag.Arg(end_index))
+	if err != nil {
+		fmt.Printf("Could not parse %s as a date: %s\n", flag.Arg(end_index), err)
+		os.Exit(2)
+	}
+
+	if s.Compare(e) == 1 {
+		direction = int(-1)
+	} else {
+		direction = int(1)
 	}
 
 	d := int64(1)
@@ -43,22 +77,15 @@ func init() {
 			fmt.Printf("Could not parse %s as a number: %s\n", flag.Arg(day_index), err)
 			os.Exit(2)
 		}
-
 		if d < 1 {
-			fmt.Printf("The incremenet %d needs to be 1 or greater\n", d)
+			fmt.Println("The step value needs to be positive")
 			os.Exit(2)
 		}
 	}
 
-	e, err := time.Parse(time.DateOnly, flag.Arg(end_index))
-	if err != nil {
-		fmt.Printf("Could not parse %s as a date: %s\n", flag.Arg(end_index), err)
-		os.Exit(2)
-	}
-
 	starting_time = s
 	ending_time = e
-	days, _ = time.ParseDuration(fmt.Sprintf("%dh", d*24))
+	days, _ = time.ParseDuration(fmt.Sprintf("%dh", int64(direction)*d*24))
 }
 
 func main() {
@@ -66,7 +93,7 @@ func main() {
 		fmt.Println(starting_time.Format(time.DateOnly))
 		starting_time = starting_time.Add(days)
 
-		if starting_time.Compare(ending_time) == 1 {
+		if starting_time.Compare(ending_time) == direction {
 			break
 		}
 	}
